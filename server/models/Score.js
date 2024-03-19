68e55bbb997ee26e23b0f9d677922ff8cabdb33a
frankiederosier <frankiederosier@Frankies-MacBook-Pro.local>
2024-03-18 20:01:18 -0400

    start of leaderboards,menus and some schema

---
+++ server/models/Score.js
@@ -0,0 +1,29 @@
+//scoreboard
+const { Schema, model } = require('mongoose');
+
+const scoreSchema = new Schema(
+  {
+    value: {
+        type: Number,
+        required: true
+    },
+    highScore: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    difficulty: {
+      type: String,
+      enum: ['beginner', 'intermediate', 'expert'],
+      required: true
+    },
+    player: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+  },
+);
+
+const Score = model('Score', scoreSchema);
+
+module.exports = Score;
