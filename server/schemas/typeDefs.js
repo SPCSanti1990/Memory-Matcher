f56f87346672dd7ea31435ceee0c1e6d06d3956e
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2024-03-19 19:21:03 -0400

    added gitignore files to server and client folders. added typeDefs to server schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server-express');
+
+// template typeDefs, subject to change
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        highScore: Int
+        lastScore: Int
+        friends: [User]!
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Score {
+        value: Int
+        highScore: Boolean
+        difficulty: String
+        player: User
+    }
+    type Query {
+        me: User
+        checkHighScore(player: ID!, difficulty: String!): Score
+        leaderboard: [Score]
+        profile: [Score]
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveScore(value: Int!, highScore: Boolean!, difficulty: String, player: ID!): Score
+        updateOldHigh(difficulty: String, player: ID!): Score
+        updatePlayerHigh(_id: ID!, highScore: Int!): User
+        lastScore(_id: ID!, lastScore: Int!): User
+        deleteScores(player: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
