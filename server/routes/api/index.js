703ee06be3b83580b2002be4ad1971a799c04ec8
frankiederosier <frankiederosier@Frankies-MBP.lan>
2024-03-24 10:05:58 -0400

    server routes

---
+++ server/routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const stripeRoutes = require('./Stripe');  // Stripe routes
+
+router.use('/users', userRoutes);
+router.use('/stripe', stripeRoutes);
+
+module.exports = router;
+
+//imports
